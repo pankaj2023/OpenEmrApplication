74c9ff64bea374b054af690a8919a6aabad6eb4f
pankaj2023 <pankajjamdade023@gmail.com>
2021-07-26 12:38:04 +0530

    Before method After method

--- OpenEMR/src/test/java/com/vfislk/test/TestLogin.java
+++ OpenEMR/src/test/java/com/vfislk/test/TestLogin.java
@@ -6,30 +6,64 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestLogin {
 
-	@Test
-	public void validCredential() {
 	
-		//System.out.println("Login Success:");
+private WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp()
+	{
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
-		
-		WebDriver driver=new ChromeDriver();	
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-		driver.get("http://demo.openemr.io/b/openemr/interface/login/login.php?site=default");
-				
-		driver.findElement(By.id("authUser")).sendKeys("admin");
-		driver.findElement(By.id("clearPass")).sendKeys("pass");
-		Select selectLanguage = new Select(driver.findElement(By.name("languageChoice")));
-		selectLanguage.selectByVisibleText("English (Indian)");
-	
-		
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.get("https://demo.openemr.io/a/openemr/index.php");
 	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+	@Test
+	public void invalidCredentialTest()
+	{		
+		driver.findElement(By.id("authUser")).sendKeys("admin123");
+		driver.findElement(By.id("clearPass")).sendKeys("pass");	
+		Select selectLanguage=new Select(driver.findElement(By.name("languageChoice")));
+		selectLanguage.selectByVisibleText("English (Indian)");		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+			
+		String actualValue= driver.findElement(By.xpath("//div[contains(text(),'Invalid')]")).getText().trim();
+		
+		Assert.assertEquals(actualValue, "Invalid username or password");
+	}
+	
+	@Test(description = "Valid Credential Test")
+	public void validCredentialTest()
+	{		
+		driver.findElement(By.id("authUser")).sendKeys("admin");
+		driver.findElement(By.id("clearPass")).sendKeys("pass");	
+		Select selectLanguage=new Select(driver.findElement(By.name("languageChoice")));
+		selectLanguage.selectByVisibleText("English (Indian)");		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		WebDriverWait wait=new WebDriverWait(driver, 50);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Calendar']")));
+		
+		String actualValue=driver.getTitle();	
+		Assert.assertEquals(actualValue,"patient OS");		
+			
+	}	
 
 }
